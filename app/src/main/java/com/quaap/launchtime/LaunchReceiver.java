0c8c822fb220c4407544ac9cbaff7ce1822975e4
Tom Kliethermes <kliethermes.tom@gmail.com>
2018-09-04 20:05:28 -0500

    clean and update

--- app/src/main/java/com/quaap/launchtime/LaunchReceiver.java
+++ app/src/main/java/com/quaap/launchtime/LaunchReceiver.java
@@ -5,7 +5,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.preference.PreferenceManager;
